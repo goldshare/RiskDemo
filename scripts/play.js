472b4f321ccdfd67fb6a2ce4ac886c0e7c2f5819
chaozh <chao@hust.edu.cn>
2015-06-17 15:32:11 +0800

    basic animation for result & load

--- scripts/play.js
+++ scripts/play.js
@@ -6,18 +6,28 @@
 		ROUNDS: 1
 	};
 	
-	$(document).ready(function(){
+	$(document).ready(function() {
 		//get compiled template function
 		var $invest = $("#invest"),
 			$result = $('#result');
 
+		var dailyInfo = {};
+
 		var investTemplate = Handlebars.compile($invest.html());
 		var resultTemplate = Handlebars.compile($result.html());
 		//firstly loads daily info into 
-		var getDailyInfo = function() {
-			var json = {};
+		var getDailyInfo = function(callback) {
+			var jqxhr = $.getJSON( "example.json")
+			.success(function(data) {
+			  	// console.log( "success" );
+			  	dailyInfo = $.extend({}, data, invest_status);
+			})
+			.fail(function() {
+				// console.log( "error" );
+				dailyInfo = $.extend({}, invest_status);
+			});
 
-			return $.extend({}, json, invest_status);
+			jqxhr.complete(callback);
 		};
 
 		//deal with every turn
@@ -26,40 +36,75 @@
 			$container.html(content);
 		}
 
+		var bindEvent = function() {
+			$('#invest-confirm-btn').click(function () {
+		  		showInvestResult();
+			});
+
+			$('[name="base-options"]').on("change", function() {
+				$("#invest-money").html(invest_status.BASE_MONEY * $(this).val() / 3);
+			});
+
+			$('[name="lever-options"]').on("change", function() {
+				console.log($(this).val());
+			});
+		}
+
 
 		var startNewRounds = function() {
-			$result.hide();
-			var json = getDailyInfo();
-			contentRefresh($invest, investTemplate, json);
-			$invest.show();
+			//loading animation
+
+			//get info
+			getDailyInfo(function() {
+				contentRefresh($invest, investTemplate, dailyInfo);
+				$result.hide();
+				$invest.show();
+				bindEvent();
+			});
 		};
 		
 		
 
 		var showInvestResult = function() {
-			$invest.hide();
-			contentRefresh($result, resultTemplate, json);
-		  	$result.show();
+			$invest.fadeOut();
+			//contentRefresh($result, resultTemplate, json);
+		  	$result.fadeIn(function() {
+		  		//play animations
 
-		  	//play animations
+		  		//progress bar animation
+		  		var $progressBar = $('#result-progress-bar');
+			    var current_perc = 0;
+			    var deferred = $.Deferred();
+				var promise = deferred.promise();
 
-		  	//real logic
-		  	++invest_status.ROUNDS;
-		  	if(invest_status.ROUNDS >= MAX_ROUNDS)
-		  		location.ref = ""; //redirect
-		  	else
-				startNewRounds();
+				var timer = setInterval( function () {
+				    if (current_perc > 100) {
+		                deferred.resolve();
+		            } else {
+		                current_perc += 1;
+		                $progressBar.css('width', (current_perc)+'%');
+		            }
+
+		            $progressBar.text((current_perc)+'%');
+				}, 50);
+
+				promise.done(function () {
+				    clearInterval( timer );
+				    //real logic & do caculation
+				  	++invest_status.ROUNDS;
+				  	if(invest_status.ROUNDS >= MAX_ROUNDS){
+				  		location.ref = ""; //redirect
+				  	}
+				  	else {
+				  		$progressBar.css('width', '0%');
+						startNewRounds();
+				  	}
+				});
+		  		
+		  	});
 		};
-
+		//do animation
 		startNewRounds();
-		
-
-		$('#invest-confirm-btn').click(function () {
-			var $btn = $(this)
-		  	$btn.button('reset');
-
-		  	showInvestResult();
-		});
 		
 	});
 	
